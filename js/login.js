aacd88deda5e37b0dcef5d3c061660e855bbb04d
Beatriz <medeirosfbia@gmail.com>
2024-10-24 22:07:07 -0300

    requisition back

--- js/login.js
+++ js/login.js
@@ -1,17 +1,46 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
+const base_url = "http://localhost:8090/user"
+
+document.getElementById('login-form').addEventListener('submit', async function(event) {
     event.preventDefault();
     
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
     
-    // Verificação simples para demonstrar
-    if (username === "user" && password === "1234") {
-        alert("Login bem-sucedido!");
-        errorMessage.style.display = "none";
-        window.location.href = "main.html";
-    } else {
-        errorMessage.style.display = "block";
-        errorMessage.textContent = "Usuário ou senha incorretos!";
-    }
+    // // Verificação simples para demonstrar
+    // if (username === "user" && password === "1234") {
+    //     alert("Login bem-sucedido!");
+    //     errorMessage.style.display = "none";
+    //     window.location.href = "main.html";
+    // } else {
+    //     errorMessage.style.display = "block";
+    //     errorMessage.textContent = "Usuário ou senha incorretos!";
+    // }
+
+    const body = `{"username": "${username}", "password": "${password}"}`;
+    
+    await $.ajax({
+        type: 'POST',
+        url: `${base_url}/login`,
+        contentType: 'application/json',
+        dataType: 'json',
+        data: body,
+        success: (res) => { 
+            console.log(res);
+            errorMessage.style.display = "none";
+            window.location.href = "main.html";
+        },
+        error: (res) => {
+            console.log(res);
+            errorMessage.style.display = "block";
+            errorMessage.textContent = "Usuário ou senha incorretos!";
+
+            // Limpa os campos de username e password
+            document.getElementById("username").value = "";
+            document.getElementById("password").value = "";
+            
+             // Foca no campo de username
+            document.getElementById("username").focus();
+        } 
+    });
 });
--- js/register.js
+++ js/register.js
@@ -1,8 +1,13 @@
+const base_url = "http://localhost:8090/user"
+
 document.getElementById('register-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const email = document.getElementById('email').value;
+    const username = document.getElementById('username').value;
     const name = document.getElementById('name').value;
+    const lastName = document.getElementById('lastName').value;
+    const birthDate = document.getElementById('birthDate').value;
     const password1Field = document.getElementById('password1');
     const password2Field = document.getElementById('password2');
     const errorMessage = document.getElementById('error-message');
@@ -18,6 +23,35 @@ document.getElementById('register-form').addEventListener('submit', function(eve
     }
     else{
         // manda para o back end
+        const body = `{
+            "username": "${username}",
+            "email" : "${email}",
+            "name" : "${name}",
+            "lastname" : "${lastName}",
+            "password": "${document.getElementById("password1").value}",
+            "birthDate" : "${birthDate}"
+            }`;
+
+            $.ajax({
+                type: 'POST',
+                url: `${base_url}/signup`,
+                contentType: 'application/json',
+                dataType: 'json',
+                data: body,
+                success: (res) => { 
+                    console.log(res);
+                    errorMessage.style.display = "none";
+                    window.location.href = "index.html";
+                },
+                error: (res) => {
+                    console.log(res);
+                    errorMessage.style.display = "block";
+                    errorMessage.textContent = "Email ou Usuário já existente";
+                    
+                     // Foca no campo de username
+                    document.getElementById("email").focus();
+                } 
+            });
     }
 
 });
